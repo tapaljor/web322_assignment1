2f898963f005b7f0b29903685cd580eb58f0e510
Tashi Paljor <tapaljor@gmail.com>
2024-05-23 17:01:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var HTTP_PORT = env.PORT || 3000;
+var HTTP_PORT = process.env.PORT || 3000;
 var express = require("express");
 
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+//actual running of server and listening to event like req and res
 app.listen(HTTP_PORT, () => {
   console.log(`Listening to port ${HTTP_PORT}`);
 });
